213957ea17badd6bdde20284b601b168d10236a9
sangjun.heo <hsj99316@naver.com>
2026-05-11 16:48:07 +0900

    feat: GKE 클러스터 및 Spot 노드풀 테라폼 코드 작성

--- infra/GKE/provider.tf
+++ infra/GKE/provider.tf
@@ -1,1 +1,13 @@
-a
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.31"
+    }
+  }
+}
+
+provider "google" {
+  project = "ilpoomjinro"
+  region  = "asia-northeast3"
+}
